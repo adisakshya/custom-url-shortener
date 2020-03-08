5b109bd50567df2e0ec01eadd4f029a59f9acdfa
Adisakshya Chauhan <45028982+adisakshya@users.noreply.github.com>
2020-03-08 12:46:23 +0530

    added shortenURL schema

---
+++ custom-url-shortner/application_server/models/shortenURL.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const schema = mongoose; 
+
+const shortenURLSchema = new schema({
+  originalUrl: {
+    type: String,
+    required: true
+  },
+  urlCode: {
+    type: String
+  },
+  shortURL: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    required: true
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+mongoose.model("shortenURL", shortenURLSchema);
