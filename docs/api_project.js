4c553d7875bab90196fc8bd3fc24eefd9e3a227a
Adisakshya Chauhan <45028982+adisakshya@users.noreply.github.com>
2020-03-24 23:19:46 +0530

    added docs/

---
+++ docs/api_project.js
@@ -0,0 +1,20 @@
+define({
+  "name": "Custom URL Shortner API Documentation",
+  "version": "1.0.0",
+  "description": "This is the documentation for the custom URL shortner service",
+  "title": "Documentation | Custom URL Shortner API",
+  "url": "http://<domain:port>",
+  "template": {
+    "withCompare": true,
+    "withGenerator": true
+  },
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2020-03-24T17:48:36.256Z",
+    "url": "http://apidocjs.com",
+    "version": "0.20.1"
+  }
+});
